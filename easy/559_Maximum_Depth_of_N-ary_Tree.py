2c85f0838a8a9e1ba5304e180641b7650b1a874e
Takeru Seki <0084takeru@gmail.com>
2025-02-11 22:30:58 +0900

    2/11

---
+++ easy/559_Maximum_Depth_of_N-ary_Tree.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        maxDepth = 0
+        def dfs(node,depth):
+          nonlocal maxDepth
+          if not node:
+            return
+          if depth > maxDepth:
+            maxDepth = depth
+          for child in node.children:
+            dfs(child, depth+1)
+        dfs(root,1)
+        return maxDepth
