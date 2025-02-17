9ce8e7465d6d8d1d6dcb3c7c7e34e258503ddaf6
Takeru Seki <0084takeru@gmail.com>
2025-02-17 22:31:44 +0900

    2/17

---
+++ medium/889_Construct_Binary_Tree_from_Preorder_and_Postorder_Traversal.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        #preorderが空の場合
+        if not preorder:
+            return
+        #部分木のrootを取り出す
+        root = TreeNode(preorder[0])
+        #preorderが一つの場合、それが葉ノード
+        if len(preorder) == 1:
+          return root
+        #左部分木のルート
+        left_root = preorder[1]
+        #左部分木のサイズ
+        left_size = postorder.index(left_root) + 1
+        root.left = self.constructFromPrePost(preorder[1:left_size+1], postorder[:left_size])
+        root.right = self.constructFromPrePost(preorder[left_size+1:], postorder[left_size:-1])
+        return root
+
+preorder = [1,2,4,5,3,6,7]
+postorder = [4,5,2,6,7,3,1]
