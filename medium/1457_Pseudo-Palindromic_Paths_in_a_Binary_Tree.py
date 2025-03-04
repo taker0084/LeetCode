065c4e38f40c4c9a37f243a58abe53a7d93e5815
Takeru Seki <0084takeru@gmail.com>
2025-03-04 23:40:07 +0900

    3/4

---
+++ medium/1457_Pseudo-Palindromic_Paths_in_a_Binary_Tree.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        count = 0
+        stack = [(root, 0)]
+
+        while stack:
+            node, path = stack.pop()
+
+            if node:
+                # 含まれている値を調べる(含まれているビットが1になっている)
+                # ^= は XORであるため、2回出てくると0に戻る
+                path ^= 1 << node.val
+
+                if not node.left and not node.right:
+                    if path & (path - 1) == 0:
+                        count += 1
+                else:
+                    stack.append((node.right, path))
+                    stack.append((node.left, path))
+
+        return count
