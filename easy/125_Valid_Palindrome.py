b48d58065a3b124d766bfa70016aa9fb536df2a4
Takeru Seki <0084takeru@gmail.com>
2025-04-08 00:06:18 +0900

    4/7

--- easy/125_Valid_Palindrome.py
+++ easy/125_Valid_Palindrome.py
@@ -1,7 +1,33 @@
 import re
+import pytest
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
+        isPalindrome returns the sentence is equal to reversed sentence or not
+
+        Args:
+            s (str): input
+
+        Returns:
+            bool: equal or not
+        """
         s = re.sub(r"[^a-zA-Z0-9]", "", s)
         words = s.split()
         s = "".join(words).lower()
         return s[::-1] == s
+#--------------TEST CASES---------------------------
+@pytest.fixture
+def solution():
+    return Solution()
+
+def test_isPalindrome(solution):
+    """
+    Test case to verify that the isPalindrome function returns true answer
+
+    Example1: true case
+    Example2: false case
+    """
+    # Example1
+    assert solution.isPalindrome("A man, a plan, a canal: Panama") == True
+    # Example2
+    assert solution.isPalindrome("race a car") == False
